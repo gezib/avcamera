374e62bb1c525f25c81307ad7f52ad9ac3567ce8
smile <smutear@gmail.com>
2022-09-29 01:40:33 +0800

    base code: first run

--- app/src/main/java/cn/letswap/citor/Interceptor.java
+++ app/src/main/java/cn/letswap/citor/Interceptor.java
@@ -8,6 +8,6 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class Interceptor implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        Log.e("letstart","lets getting start");
+        Log.e("lets start",lpparam.packageName);
     }
 }
